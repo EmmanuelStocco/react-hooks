657d63726194d33be9c3078ed564e92a595be7c6
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2021-03-24 01:34:53 +0000

    23

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,21 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState('initialName') //criando variavel name de estado
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
+    //name = event.target.value
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value="fulano"/> 
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +25,9 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName = "Sicrano"/>
 }
 
 export default App
+
+//<!--HandreChange é uma função q detecta alteração e faz a atualização na var de estado >
